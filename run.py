80ebc538178b132094155a784e4956f61d633472
aegis <aegis@a68c7e1a-86db-0310-a1c4-e68a6bf46cff>
2004-09-20 07:17:15 +0000

    Adding a --all option to run.py.
    git-svn-id: file:///home/chad/tmp/glscry/trunk/glscry@77 a68c7e1a-86db-0310-a1c4-e68a6bf46cff

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
+#!/usr/bin/env python
+
 import os
 import string
 import sys
+from glob import glob
+from optparse import OptionParser
 
+# Parse options.
+parser = OptionParser()
+parser.add_option('-a', '--all', action='store_true', dest='all',
+                  help='Run all tests')
+(options, args) = parser.parse_args()
+
+# Modify PYTHONPATH
 pythonpath = os.environ.get('PYTHONPATH')
 if pythonpath:
     pythonpath = pythonpath + ':' + os.getcwd()
@@ -9,11 +20,21 @@ else:
     pythonpath = os.getcwd()
 os.environ['PYTHONPATH'] = pythonpath
 
-tests = map(os.path.abspath, sys.argv[1:])
+# Get test list.
+tests = []
+if options.all:
+    tests.extend(glob('test/*.py'))
+tests = map(os.path.abspath, tests)
+
+# Create data directory if it does not exist and switch to it.
 try:
     os.mkdir('data')
 except OSError:
     pass
 os.chdir('data')
-    
+
+if not tests:
+    print "No tests specified.  Specify a test explicitly or use -a."
+    sys.exit(1)
+
 os.system(sys.executable + ' ' + string.join(tests))
