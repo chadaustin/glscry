1dd50f1ab459d51206f9b5385a4e121e44bc41b3
aegis <aegis@a68c7e1a-86db-0310-a1c4-e68a6bf46cff>
2004-10-31 20:27:35 +0000

    Simplified Christian's test more.
    git-svn-id: file:///home/chad/tmp/glscry/trunk/glscry@119 a68c7e1a-86db-0310-a1c4-e68a6bf46cff

--- prototypes/vcache/main.cpp
+++ prototypes/vcache/main.cpp
@@ -6,56 +6,64 @@
 #endif
 #include <GL/gl.h>
 #include <SDL.h>
+#include <assert.h>
 #include <stdio.h>
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const int INDEX_COUNT = 10000;
+
 struct Vertex{
-    float x,y,z;
-    long c;
-    float u,v;
+    GLfloat v[3];
+//    GLubyte c[4];
+//    GLfloat t[2];
+
+    static void checkSize() {
+        //assert(sizeof(Vertex) == sizeof(GLfloat) * 5 + sizeof(GLubyte) * 4);
+        assert(sizeof(Vertex) == sizeof(GLfloat) * 3);
+    }
 };
 
 void test ( )
 {
     Vertex data[8192];
-    unsigned short indices[10000];
-    int err;
+    GLushort indices[INDEX_COUNT];
 
     for ( int i=0; i < 8192; ++i )
     {
-        data[i].x = 0.001f*(float)(i/2);
-        data[i].y = 0.001f*(float)(i%2);
-        data[i].z = 0.0f;
-        data[i].c = 0xffff00ff;
-        data[i].u = 0.0f;
-        data[i].v = 0.0f;
+        data[i].v[0] = 0.0f;
+        data[i].v[1] = 0.0f;
+        data[i].v[2] = 0.0f;
+#if 0
+        data[i].v[0] = 0.001f*(float)(i/2);
+        data[i].v[1] = 0.001f*(float)(i%2);
+        data[i].v[2] = 0.0f;
+        data[i].c[0] = 0xff;
+        data[i].c[1] = 0x00;
+        data[i].c[2] = 0xff;
+        data[i].c[3] = 0xff;
+        data[i].t[0] = 0.0f;
+        data[i].t[1] = 0.0f;
+#endif
     }
 
-    glDisable ( GL_LIGHTING );
-    glDisable ( GL_BLEND );
-    glDisable ( GL_TEXTURE_2D );
-    glDisable ( GL_TEXTURE_1D );
-    glDisable ( GL_DEPTH_TEST );
-    glDisable ( GL_STENCIL_TEST );
+    //glDisable ( GL_LIGHTING );
+    //glDisable ( GL_BLEND );
+    //glDisable ( GL_TEXTURE_2D );
+    //glDisable ( GL_TEXTURE_1D );
+    //glDisable ( GL_DEPTH_TEST );
+    //glDisable ( GL_STENCIL_TEST );
 
-    glClearColor ( 0, 0, 0.3f, 0);
-    err=glGetError ();
-    glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    err=glGetError ();
-    glViewport ( 0, 0, 800, 600 );
-    err=glGetError ();
+    //glClearColor ( 0, 0, 0.3f, 0);
+    //glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    //glViewport ( 0, 0, WIDTH, HEIGHT );
 
     glEnableClientState ( GL_VERTEX_ARRAY );
-    err=glGetError ();
-    glEnableClientState ( GL_COLOR_ARRAY );
-    err=glGetError ();
-    glEnableClientState ( GL_TEXTURE_COORD_ARRAY );
-    err=glGetError ();
-    glColorPointer ( 4, GL_UNSIGNED_BYTE, sizeof(Vertex), &data[0].c );
-    err=glGetError ();
-    glVertexPointer ( 3, GL_FLOAT, sizeof(Vertex), &data[0].x );
-    err=glGetError ();
-    glTexCoordPointer ( 2, GL_FLOAT, sizeof(Vertex), &data[0].u );
-    err=glGetError ();
+//    glEnableClientState ( GL_COLOR_ARRAY );
+//    glEnableClientState ( GL_TEXTURE_COORD_ARRAY );
+    glVertexPointer ( 3, GL_FLOAT, sizeof(Vertex), data[0].v );
+//    glColorPointer ( 4, GL_UNSIGNED_BYTE, sizeof(Vertex), data[0].c );
+//    glTexCoordPointer ( 2, GL_FLOAT, sizeof(Vertex), data[0].t );
 
     for ( int count=2; count<256; count+=2 )
     {
@@ -65,12 +73,12 @@ void test ( )
         long t_start=SDL_GetTicks();
 		
         int loop=0;
-        for (; loop<1000; ++loop )
-            glDrawElements ( GL_TRIANGLE_STRIP, 10000, GL_UNSIGNED_SHORT, &indices );
-        glFlush ();
+        for (; loop<1000; ++loop)
+            glDrawElements(GL_TRIANGLES, INDEX_COUNT, GL_UNSIGNED_SHORT, &indices);
+        glFinish ();
 		
         long t_delta=SDL_GetTicks() - t_start;
-        float rate = ((float)(loop*10000))*1000.0f/t_delta;
+        float rate = ((float)(loop*INDEX_COUNT))*1000.0f/t_delta;
         printf ("%3d : %f vertices/s\n", count, rate );
     }
 
@@ -85,23 +93,24 @@ int main ( int argc, char ** argv )
     SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 5 );
     SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 16 );
     SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );	
-    SDL_SetVideoMode ( 800, 600, 32, SDL_OPENGL );
+    SDL_SetVideoMode ( WIDTH, HEIGHT, 32, SDL_OPENGL );
 
+    test ( );
+/*
     SDL_Event ev;
     bool quit=false;
 
-    test ( );
-/*	do
-  {
-  if ( SDL_PollEvent ( &ev ) )
-  {
-  if ( ev.type == SDL_KEYDOWN )
-  if ( ev.key.keysym.sym == SDLK_q )
-  quit = true;
+    do
+    {
+    if ( SDL_PollEvent ( &ev ) )
+    {
+    if ( ev.type == SDL_KEYDOWN )
+    if ( ev.key.keysym.sym == SDLK_q )
+    quit = true;
 			
-  }
+    }
 
-  } while ( !quit );
+    } while ( !quit );
 */
     SDL_Quit ( );
 	
