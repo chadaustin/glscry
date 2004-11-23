44999ad6b401f12cf13c9ae8803f50215db3d704
aegis <aegis@a68c7e1a-86db-0310-a1c4-e68a6bf46cff>
2004-11-23 01:37:50 +0000

    Forgot to add the statechange test.
    git-svn-id: file:///home/chad/tmp/glscry/trunk/glscry@125 a68c7e1a-86db-0310-a1c4-e68a6bf46cff

---
+++ test/statechange.py
@@ -0,0 +1,51 @@
+from glscry import *
+from math import *
+
+def buildStateSet():
+    state = TextureState()
+    state.texture = Texture()
+
+    stateSet = StateSet()
+    stateSet.setState(state)
+    return stateSet
+
+TestType = VertexArrayTest
+state1 = buildStateSet()
+state2 = buildStateSet()
+finishAction = FinishAction()
+
+geo = buildGeometry(GL_TRIANGLES)
+testNoChange       = TestType("NoChange",       geo)
+testChange         = TestType("Change",         geo)
+testNoChangeFull   = TestType("NoChangeFull",   geo)
+testChangeFull     = TestType("ChangeFull",     geo)
+testNoChangeFinish = TestType("NoChangeFinish", geo)
+testChangeFinish   = TestType("ChangeFinish",   geo)
+
+for t in [testNoChange, testNoChangeFull, testNoChangeFinish]:
+    t.addStateSet(state1)
+
+for t in [testChange, testChangeFull, testChangeFinish]:
+    t.addStateSet(state1)
+    t.addStateSet(state2)
+
+for t in [testNoChangeFull, testChangeFull]:
+    t.fullStateSwitch = True
+
+for t in [testNoChangeFinish, testChangeFinish]:
+    t.addAction(finishAction)
+
+testList = [
+    testNoChange,
+    testChange,
+    testNoChangeFull,
+    testChangeFull,
+    testNoChangeFinish,
+    testChangeFinish]
+
+power = 1.2
+start = floor(log(1) / log(power))
+end   = ceil(log(1024) / log(power))
+range = uniquePowerRange(start, end, power)
+
+runTestsRange("statechange.data", testList, 1, "VertexRate", "BatchSize", range)
