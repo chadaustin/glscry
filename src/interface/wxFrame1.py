0dfcfe4abeaa7befeab887d604ec152285db1738
aallen86 <aallen86@a68c7e1a-86db-0310-a1c4-e68a6bf46cff>
2005-03-31 20:51:06 +0000

    git-svn-id: file:///home/chad/tmp/glscry/trunk/glscry@221 a68c7e1a-86db-0310-a1c4-e68a6bf46cff

--- src/interface/wxFrame1.py
+++ src/interface/wxFrame1.py
@@ -7,14 +7,13 @@ import Gnuplot, Gnuplot.PlotItems, Gnuplot.funcutils
 def create(parent):
     return wxFrame1(parent)
 
-[wxID_WXFRAME1, wxID_WXFRAME1BROWSE_BUTTON, wxID_WXFRAME1BATCHSIZES, 
- wxID_WXFRAME1GRAPH_BUTTON, wxID_WXFRAME1NOTEBOOK1, wxID_WXFRAME1PANEL1, 
- wxID_WXFRAME1PANEL2, wxID_WXFRAME1STATICBITMAP1, wxID_WXFRAME1TEXTCTRL1, 
- wxID_WXFRAME1TREECTRL1, wxID_WXFRAME1WINDOW1,wxID_WXFRAME1EXAMPLE,
- wxID_WXFRAME1HIERZ, wxID_WXFRAME1LIGHTS, wxID_WXFRAME1PIXELTRANSFER,
- wxID_WXFRAME1TEXMEM, wxID_WXMINIFRAME1SCROLLEDWINDOW1, wxID_WXMINIFRAME1,
- wxID_WXFULLBITMAP 
-] = map(lambda _init_ctrls: wxNewId(), range(19))
+[wxID_WXFRAME1, wxID_WXFRAME1BATCHSIZES, wxID_WXFRAME1BROWSE_BUTTON, 
+ wxID_WXFRAME1CHOICE1, wxID_WXFRAME1EXAMPLE, wxID_WXFRAME1GRAPH_BUTTON, 
+ wxID_WXFRAME1HIERZ, wxID_WXFRAME1LIGHTS, wxID_WXFRAME1NOTEBOOK1, 
+ wxID_WXFRAME1PANEL1, wxID_WXFRAME1PANEL2, wxID_WXFRAME1PIXELTRANSFER, 
+ wxID_WXFRAME1STATICBITMAP1, wxID_WXFRAME1STATICTEXT1, wxID_WXFRAME1TEXMEM, 
+ wxID_WXFRAME1TEXTCTRL1, wxID_WXFRAME1TREECTRL1, wxID_WXFRAME1WINDOW1, 
+] = map(lambda _init_ctrls: wxNewId(), range(18))
 
 [wxID_WXFRAME1MENU1ID_EXIT, wxID_WXFRAME1MENU1ID_NEW, 
  wxID_WXFRAME1MENU1ID_OPEN, wxID_WXFRAME1MENU1ID_SAVE, 
@@ -86,8 +85,15 @@ class wxFrame1(wxFrame):
 
         parent.AddPage(imageId=-1, page=self.panel1, select=True,
               text=u'Analyze')
-        parent.AddPage(imageId=-1, page=self.panel2, select=False, text='GLScry')
-
+        parent.AddPage(imageId=-1, page=self.panel2, select=False,
+              text='GLScry')
+              
+    def _init_choiceBox(self, parent):
+        parent.Append("VertexRate")
+        parent.Append("PrimitiveRate")
+        parent.Append("FillRate")
+        parent.Append("BatchRate")
+    
     def _init_utils(self):
         # generated method, don't edit
         self.menuBar1 = wxMenuBar()
@@ -109,7 +115,7 @@ class wxFrame1(wxFrame):
     def _init_ctrls(self, prnt):
         # generated method, don't edit
         wxFrame.__init__(self, id=wxID_WXFRAME1, name='', parent=prnt,
-              pos=wxPoint(-4, -4), size=wxSize(1032, 746),
+              pos=wxPoint(112, 70), size=wxSize(1032, 746),
               style=wxDEFAULT_FRAME_STYLE, title='work_in_progress')
         self._init_utils()
         self.SetClientSize(wxSize(1024, 712))
@@ -130,22 +136,22 @@ class wxFrame1(wxFrame):
               style=wxTAB_TRAVERSAL)
 
         self.textCtrl1 = wxTextCtrl(id=wxID_WXFRAME1TEXTCTRL1, name='textCtrl1',
-              parent=self.panel1, pos=wxPoint(8, 16), size=wxSize(144, 21),
+              parent=self.panel1, pos=wxPoint(8, 48), size=wxSize(144, 21),
               style=0, value=u'Please select a directory')
 
         self.browse_button = wxButton(id=wxID_WXFRAME1BROWSE_BUTTON,
               label=u'Browse', name=u'browse_button', parent=self.panel1,
-              pos=wxPoint(160, 16), size=wxSize(75, 23), style=0)
+              pos=wxPoint(160, 48), size=wxSize(75, 23), style=0)
         EVT_BUTTON(self.browse_button, wxID_WXFRAME1BROWSE_BUTTON,
               self.BuildFolder)
 
         self.treeCtrl1 = wxTreeCtrl(id=wxID_WXFRAME1TREECTRL1, name='treeCtrl1',
-              parent=self.panel1, pos=wxPoint(8, 56), size=wxSize(216, 536),
+              parent=self.panel1, pos=wxPoint(8, 80), size=wxSize(216, 536),
               style=wxTR_HAS_BUTTONS | wxTR_MULTIPLE)
 
         self.graph_button = wxButton(id=wxID_WXFRAME1GRAPH_BUTTON,
               label=u'Graph', name=u'graph_button', parent=self.panel1,
-              pos=wxPoint(160, 608), size=wxSize(75, 31), style=0)
+              pos=wxPoint(160, 624), size=wxSize(75, 31), style=0)
         EVT_BUTTON(self.graph_button, wxID_WXFRAME1GRAPH_BUTTON,
               self.OnGraphButton)
 
@@ -161,28 +167,38 @@ class wxFrame1(wxFrame):
               label='Batch Sizes', name='batchsizes', parent=self.panel2,
               pos=wxPoint(64, 88), size=wxSize(83, 23), style=0)
 
-        self.example = wxCheckBox(id=wxID_WXFRAME1EXAMPLE,
-              label='Example', name='example', parent=self.panel2,
-              pos=wxPoint(64, 108), size=wxSize(83, 23), style=0)
-        
-        self.hierz = wxCheckBox(id=wxID_WXFRAME1HIERZ,
-              label='Hierz', name='hierz', parent=self.panel2,
-              pos=wxPoint(64, 128), size=wxSize(83, 23), style=0)
-        
-        self.lights = wxCheckBox(id=wxID_WXFRAME1LIGHTS,
-              label='Lights', name='lights', parent=self.panel2,
-              pos=wxPoint(64, 148), size=wxSize(83, 23), style=0)
-        
+        self.example = wxCheckBox(id=wxID_WXFRAME1EXAMPLE, label='Example',
+              name='example', parent=self.panel2, pos=wxPoint(64, 108),
+              size=wxSize(83, 23), style=0)
+
+        self.hierz = wxCheckBox(id=wxID_WXFRAME1HIERZ, label='Hierz',
+              name='hierz', parent=self.panel2, pos=wxPoint(64, 128),
+              size=wxSize(83, 23), style=0)
+
+        self.lights = wxCheckBox(id=wxID_WXFRAME1LIGHTS, label='Lights',
+              name='lights', parent=self.panel2, pos=wxPoint(64, 148),
+              size=wxSize(83, 23), style=0)
+
         self.pixeltransfer = wxCheckBox(id=wxID_WXFRAME1PIXELTRANSFER,
               label='Pixel Transfer', name='pixeltransfer', parent=self.panel2,
-              pos=wxPoint(64, 168), size=wxSize(83, 23), style=0)       
-              
-        self.texmem = wxCheckBox(id=wxID_WXFRAME1TEXMEM,
-              label='Texmem', name='texmem', parent=self.panel2,
-              pos=wxPoint(64, 188), size=wxSize(83, 23), style=0)           
-        
-        self._init_coll_notebook1_Pages(self.notebook1)
+              pos=wxPoint(64, 168), size=wxSize(83, 23), style=0)
 
+        self.texmem = wxCheckBox(id=wxID_WXFRAME1TEXMEM, label='Texmem',
+              name='texmem', parent=self.panel2, pos=wxPoint(64, 188),
+              size=wxSize(83, 23), style=0)
+        
+        self.choice1 = wxChoice(choices=[], id=wxID_WXFRAME1CHOICE1,
+              name='choice1', parent=self.panel1, pos=wxPoint(128, 16),
+              size=wxSize(109, 21), style=0)
+
+        self.staticText1 = wxStaticText(id=wxID_WXFRAME1STATICTEXT1,
+              label=u'First choose a data type:', name='staticText1',
+              parent=self.panel1, pos=wxPoint(8, 16), size=wxSize(116, 16),
+              style=0)
+              
+        self._init_choiceBox(self.choice1)
+        self._init_coll_notebook1_Pages(self.notebook1)
+        
     def __init__(self, parent,filename='work_in_progress'):
         self.dirname=""
         self._init_ctrls(parent)
@@ -215,6 +231,9 @@ class wxFrame1(wxFrame):
             self.OnSave()
         dlg.Destroy()    
     def OnMenu1Id_exitMenu(self, event):
+        event.Skip()
+        event.Skip()
+        
         #-#------Exit------#-#
         self.Close(true)
     
@@ -229,7 +248,7 @@ class wxFrame1(wxFrame):
             path = self.treeCtrl1.GetPyData(selected[count])
             count = count + 1
             self.SetGraphLabel(path, g)
-                       
+            self.GraphData(path, g)           
     def SaveAndDisplay(self, event):
         #-#------Graph Button Event------#-#
         selected = self.treeCtrl1.GetSelection()
@@ -307,7 +326,7 @@ class wxFrame1(wxFrame):
             if os.path.isfile(pathinquestion):
                 extension = os.path.splitext(pathinquestion)
                 extension = extension[1]
-                if extension == ".gnuplot":
+                if extension == ".testresult":
                     child = self.treeCtrl1.AppendItem(parent, listing)
                     childdata = self.treeCtrl1.GetItemData(child)
                     childdata.path = pathinquestion
@@ -320,22 +339,40 @@ class wxFrame1(wxFrame):
                 self.BuildChildrenFromDir(newparent, newdir)
                   
     def SetGraphLabel(self, path, graph):
-        # load info from .gnuplot file
-        file = open(path)
-        line = file.readline()
-        for line in file:
-            returnchar = file.readline()
-            graph(line)
-        # plot .data file assosciated with the .gnuplot file
-        extension = os.path.splitext(path)
-        extension = extension[1]
-        path = path.replace(extension, '.data')
-        # replace the filename.data with full path
-        name = path.split('\\')
-        size = len(name)
-        filename = name[size-1]
-        line = line.replace(filename, path)
-        line = line.replace("\\","\\\\", -1)
-        graph(line)
-        raw_input('Please press return to continue...\n')    
-                       
+        choice = self.choice1.GetSelection()
+        if choice == -1:
+            choice = 0
+        # load info from .testresult file
+        file = eval(open(path).read())
+        # set x-label
+        X = file['Test']['XUnits']
+        graph.xlabel(X)
+        # set y-label
+        Y = file['Test']['GraphLines'][0]['ResultSet'][0]['Results'][choice]['Units']
+        graph.ylabel(Y)
+        # set the title
+        version = file['GLScry Version']
+        host = file['System']['Host']
+        vendor = file['System']['OpenGL Vendor']
+        renderer = file['System']['OpenGL Renderer']
+        glversion = file['System']['OpenGL Version']
+        Gtitle = version + " :: " + host + " :: " + vendor + " :: " + renderer + " :: " + glversion
+        graph.title(Gtitle)
+        
+    def GraphData(self, path, graph):
+        file = eval(open(path).read())
+        choice = self.choice1.GetSelection()
+        if choice == -1:
+            choice = 0
+        size = len(file['Test']['GraphLines'][0]['ResultSet'])
+        count = 0
+        while count < size:
+            name = file['Test']['GraphLines'][0]['ResultSet'][count]['Name']
+            result = file['Test']['GraphLines'][0]['ResultSet'][count]['Results'][choice]['Value']
+            # graph it
+            print name
+            print result
+            count = count + 1
+        graph.reset()
+
+
